bafc53f14ff4af0896e043bd77b150760f746a09
rcwoshimao <mantoumiaoshen@gmail.com>
2022-09-13 20:02:48 -0700

    Simple flashcard lab done

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -12,8 +12,42 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        card.layer.cornerRadius = 20.0;
+        card.layer.shadowRadius = 15.0;
+        card.layer.shadowOpacity = 0.2;
+        backLabel.clipsToBounds = true;
+        frontLabel.clipsToBounds = true;
+       
+        
     }
 
+    @IBOutlet weak var backLabel: UILabel!
+    
+    @IBOutlet weak var frontLabel: UILabel!
+    
+    @IBOutlet weak var Btn1: UIButton!
 
+    
+    @IBOutlet weak var Btn2: UIButton!
+    
+    @IBOutlet weak var Btn3: UIButton!
+    
+    
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+        frontLabel.isHidden = !frontLabel.isHidden
+    }
+        
+    /* Or: @IBAction func showBtn(sender: AnyObject) {
+     if myHiddenText.hidden == true {
+         myHiddenText.hidden = false
+     } else {
+         myHiddenText.hidden = true
+     }
+ }*/
+    
+    @IBOutlet weak var card: UIView!
+    
+    
 }
+
 
